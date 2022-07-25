5f14128b2b42bc12a9954ff3bd658188808e63eb
Abdel Wadoud <abdelwadoudrasmi@gmail.com>
2022-07-25 18:54:51 +0200

    test methods update

--- src/main/java/com/blacksoft/storagemanager/Main.java
+++ src/main/java/com/blacksoft/storagemanager/Main.java
@@ -14,12 +14,9 @@ public class Main {
         System.out.println("Enter File full path: ");
         String path = scanner.nextLine();
         File file = new File(path);
-        long time = System.currentTimeMillis();
         FileSaver
                 .get()
-                .saveAsync(path, StorageUtils.randomNameWithExtension("rasmi", MimeType.IMAGE_PNG), (success, file1) -> {
-                    System.out.println(file1);
-                });
-        System.out.println(System.currentTimeMillis()-time);
+                .saveAsync(path, StorageUtils.randomNameWithExtension("rasmi", MimeType.IMAGE_PNG),
+                        (success, file1) -> System.out.println(file1));
     }
 }
