1aad35cb4d052ac87817f07a2337528f07add1d0
Abdel Wadoud <abdelwadoudrasmi@gmail.com>
2022-07-30 15:42:27 +0200

    Added a demo

---
+++ demo/Demo/src/main/java/Main.java
@@ -0,0 +1,22 @@
+import com.beastwall.storagemanager.FileSaver;
+
+/**
+ * @author AbdelWadoud Rasmi
+ * <p>
+ * Main entry point for tis java program
+ */
+public class Main {
+
+
+    public static void main(String[] args) {
+        /**
+         * Construct a FileSaver object
+         */
+        FileSaver saver = FileSaver.get();
+
+
+
+    }
+
+
+}
